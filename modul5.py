ae931d30a313bb669e2a4807ae1dafdf39520ca7
NortonHaglind <norton.haglindhilbers@elev.ga.ntig.se>
2023-10-03 09:45:12 +0200

    modul_5

--- modul5.py
+++ modul5.py
@@ -9,10 +9,37 @@ for list in list:
     print(list)
 
 
-
+active=True
 list1=[]
-while True:   
-    list1.append(input("skriv nåt roligt (sluta=q)(ta bort=x) "))
+while active:
+    name=input("skriv nåt roligt (sluta=q)(ta bort=x) ")
+    if name=="x":
+        while True:
+            try:
+                tabort=int(input(f"vilken vill du tabort? "))
+                if 1<=tabort <= len(list1):
+                    list1.pop(tabort-1)
+                    if len(list1)==0:
+                        print("slut på ord")
+                        active=False
+                        break
+                    else:    
+                        print(list1)
+                        break
+                else:
+                    print(f"skriv rätt")
+                    continue
+            except:
+                print(f"skriv siffra")
+                continue
+    elif name=="q":
+        break
+    else:
+        list1.append(name)
+        print(list1)
+        print(len(list1))
+"""while True:   
+    list1.append(input("skriv nåt roligt (sluta=q)(ta bort=x) ")) #du gillade inte min lösning#du gillade inte min lösning
     
     if "x" in list1:
         while True:
@@ -33,7 +60,7 @@ while True:
         break
     else:
         print(list1)
-        print(len(list1))
+        print(len(list1))"""
 
     
 my_library={"name": "Volvo 740" , "utg.": 1984 , "color": "vit" }
